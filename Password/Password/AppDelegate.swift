500d431e59aa5efe8d8787f4980685bc0796f7ed
Liam Ratcliffe <liam.ratcliffe@btinternet.com>
2022-10-28 10:57:00 +0100

    feat: Create new password project

---
+++ Password/Password/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Password
+//
+//  Created by Liam Ratcliffe on 28/10/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
+
---
+++ Password/Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Liam Ratcliffe on 28/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
